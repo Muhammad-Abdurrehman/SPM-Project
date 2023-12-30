2aeb99c1c201d77570afcf16047e1beae644a317
Muhammad Abdurrehman <abdurrehmanayub29@gmail.com>
2023-12-30 19:21:12 +0500

    test

--- testing.py
+++ testing.py
@@ -4,6 +4,7 @@ import requests
 username = 'abdurrehmanayub01'
 token = '2653c6ee615d1153fb7087520ae9965fd27037ac'
 console_id=31689036
+domain_name="AbdurrehmanAyub01.pythonanywhere.com"
 
 # Step 1: Fetch the list of consoles
 input_url = f'https://www.pythonanywhere.com/api/v0/user/{username}/consoles/{console_id}/send_input/'
@@ -21,31 +22,7 @@ response_cd = requests.post(input_url, headers=headers, json=git_pull_payload)
 if response_cd.status_code == 200:
     print('Code Updated')
 
-# if response_consoles.status_code == 200:
-#     consoles = response_consoles.json()['result']
-#     if consoles:
-#         # Choose the first console (you may need to adjust this based on your setup)
-#         console_id = consoles[0]['id']
-
-#         # Step 2: Send command to change directory
-#         change_directory_command = 'cd /home/{}/SPM-Project'.format(username)
-#         change_directory_payload = {'input': change_directory_command + '\n'}
-#         change_directory_input_url = f'https://www.pythonanywhere.com/api/v0/user/{username}/consoles/{console_id}/send_input/'
-#         response_cd = requests.post(change_directory_input_url, headers=headers, json=change_directory_payload)
-
-#         if response_cd.status_code == 200:
-#             print('Changed directory successfully.')
-
-#             # Step 3: Send command to perform git pull
-#             git_pull_command = 'git pull origin main'  # Adjust the branch name as needed
-#             git_pull_payload = {'input': git_pull_command + '\n'}
-#             git_pull_input_url = f'https://www.pythonanywhere.com/api/v0/user/{username}/consoles/{console_id}/send_input/'
-#             response_git_pull = requests.post(git_pull_input_url, headers=headers, json=git_pull_payload)
-
-#             print('Git pull response:', response_git_pull.status_code, response_git_pull.content)
-#         else:
-#             print('Error changing directory:', response_cd.status_code, response_cd.content)
-#     else:
-#         print('No consoles available.')
-# else:
-#     print('Error fetching consoles:', response_consoles.status_code, response_consoles.content)
+web_app_url = f'https://www.pythonanywhere.com/api/v0/user/{username}/webapps/{domain_name}/reload/'
+response = requests.post(web_app_url, headers=headers)
+if response.status_code == 200:
+    print("Website reloaded successfully")
